33a113a000001045ada53c1573df43290585d6eb
yayas0000 <128485975+yayas0000@users.noreply.github.com>
2024-07-29 09:11:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-Alternativas");
+const caixaRescultado = document.querySelector(".caixa-Resultado");
+const textoRescultado = document.querySelector(".texto-Resultado");
+
+const perguntas = [
+    enunciado: "Perguntta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2",
+    ]
+    
+    "Pergunta 2"
+]
